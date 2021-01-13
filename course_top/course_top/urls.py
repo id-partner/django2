d4a89bc9693831291566780af6e7f13d21d42e4c
Igor Drozdov <77234747+id-partner@users.noreply.github.com>
2021-01-13 14:58:17 +0500

    polls/ templates/ staticfiles/ lesson3

--- course_top/course_top/urls.py
+++ course_top/course_top/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from polls.views import index, detail, blog_hedler
+
+
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', index),
+    path('polls/<int:question_id>/', detail),
+    path('blog/', blog_hedler),
     path('admin/', admin.site.urls),
-]
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+# urlpatterns = [
+#     path('polls/', include('polls.urls')),
+#     path('admin/', admin.site.urls),
+# ]
