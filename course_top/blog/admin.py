6859cc1bf276a0ddf57302934ccb40e9948dd55b
Igor Drozdov <77234747+id-partner@users.noreply.github.com>
2021-01-30 13:40:09 +0500

    7 урок/Настройка админки

--- course_top/blog/admin.py
+++ course_top/blog/admin.py
@@ -1,14 +1,50 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
+from django.utils.html import format_html
 from .models import *
 
 
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content', 'short_description')
+    list_display = ('name', 'is_published', 'pub_date', 'image_code',)
+    list_filter = ('is_published', 'pub_date', 'categories',)
+    list_editable = ('is_published',)
+    search_fields = ('name',)
+
+    def image_code(self, object):
+        return format_html('<img src="{}" style="max-width: 50px" />',
+                           object.main_image.url
+                           )
 
 
-admin.site.register(Category)
-admin.site.register(Tag)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'in_menu', 'order', 'posts_count')
+    list_filter = ('in_menu', 'order',)
+    search_fields = ('name',)
+    list_editable = ('order',)
+    readonly_fields = ('order',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('post_set')
+
+    def posts_count(self, object):
+        return object.post_set.all().count()
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'posts_count',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('post_set')
+
+    def posts_count(self, object):
+        return object.post_set.all().count()
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Newsletter)
--- course_top/listing/admin.py
+++ course_top/listing/admin.py
@@ -1,15 +1,58 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
+from django.utils.html import format_html
 from .models import *
 
 
-class CategoryAdmin(SummernoteModelAdmin):  # Класс для HTML редактора для модели Пост
-    summernote_fields = ('content', 'description')
+def set_discount_status(modeladmin, request, queryset):
+    for object in queryset:
+        object.discount=True
+        object.save()
+
+set_discount_status.short_description= 'Проставить статус скидки всем курсам'
+
+
+class CategoryAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content', 'description',)
+    list_display = ('name', 'in_menu', 'order', 'parent', 'course_count')
+    list_editable = ('in_menu', 'order',)
+    search_fields = ('name',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('course_set')
+
+    def course_count(self, object):
+        return object.course_set.all().count()
+
+
+class SchoolCourseInLine(admin.TabularInline):
+    model = Course
+    exclude = ('description', 'categories', '')
+
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('logo_code', 'name', 'link',)
+    list_display_links = ('logo_code', 'name',)
+    search_fields = ('name',)
+    inlines = (SchoolCourseInLine,)
+
+    def logo_code(self, object):
+        return format_html('<img src="{}" style="max-width: 100px" />',
+                           object.logo.url
+                           )
+
+
+class CourselAdmin(admin.ModelAdmin):
+    list_display = ('name', 'school', 'start_date', 'price', 'discount',)
+    list_filter = ('categories', 'school',)
+    search_fields = ('name', 'school',)
+    actions = (set_discount_status,)
 
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(School)
+admin.site.register(School, SchoolAdmin)
 admin.site.register(Features)
-admin.site.register(Course)
+admin.site.register(Course, CourselAdmin)
 admin.site.register(Review)
 admin.site.register(CourseFormat)
